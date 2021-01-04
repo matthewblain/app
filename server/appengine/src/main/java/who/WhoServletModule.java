db3c95a3db78c57cb5a033af8294fb150a62696e
matthewblain <mb7oo@hotmail.com>
2021-01-03 17:21:01 -0800

    OpenCensus functional

--- server/appengine/src/main/java/who/WhoServletModule.java
+++ server/appengine/src/main/java/who/WhoServletModule.java
@@ -1,12 +1,18 @@
 package who;
 
-import static who.ForwardingServlet.forwardTo;
-
-import com.google.apphosting.utils.remoteapi.RemoteApiServlet;
+import com.google.appengine.api.modules.ModulesService;
+import com.google.appengine.api.modules.ModulesServiceFactory;
 import com.google.inject.Provides;
 import com.google.inject.servlet.ServletModule;
 import com.googlecode.objectify.ObjectifyFilter;
 import com.googlecode.objectify.ObjectifyService;
+import io.opencensus.exporter.stats.stackdriver.StackdriverStatsConfiguration;
+import io.opencensus.exporter.stats.stackdriver.StackdriverStatsExporter;
+import io.opencensus.metrics.LabelKey;
+import io.opencensus.metrics.LabelValue;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +25,35 @@ public class WhoServletModule extends ServletModule {
   private static final Logger logger = LoggerFactory.getLogger(
     WhoServletModule.class
   );
+
+  Map<LabelKey, LabelValue> getOpenCensusDefaultLabels() {
+    Map<LabelKey, LabelValue> resourceLabels = new HashMap<LabelKey, LabelValue>();
+    ModulesService modulesService = ModulesServiceFactory.getModulesService();
+    resourceLabels.put(
+      LabelKey.create("module_id", "App Engine Module ID"),
+      LabelValue.create(modulesService.getCurrentModule())
+    );
+    resourceLabels.put(
+      LabelKey.create("version_id", "App Engine Version ID"),
+      LabelValue.create(modulesService.getCurrentVersion())
+    );
+    resourceLabels.put(
+      LabelKey.create("instance_id", "App Engine Instance ID"),
+      LabelValue.create(modulesService.getCurrentInstanceId())
+    );
+
+    // This may not be necessary. The opencensus_task appears to exist so that if
+    // no other labels exist, there's a chance of unique labels-per-process.
+    final LabelKey OPENCENSUS_TASK_KEY = LabelKey.create(
+      "opencensus_task",
+      "Opencensus task identifier"
+    );
+    resourceLabels.put(
+      OPENCENSUS_TASK_KEY,
+      LabelValue.create("java@" + modulesService.getCurrentInstanceId())
+    );
+    return resourceLabels;
+  }
 
   @Provides
   Environment provideEnvironment() {
@@ -47,5 +82,17 @@ public class WhoServletModule extends ServletModule {
 
     // Set up Present RPC
     filter("/*").through(WhoRpcFilter.class);
+
+    // Set up stackdriver Google Cloud Monitoring
+
+    try {
+      StackdriverStatsConfiguration.Builder config = StackdriverStatsConfiguration.builder();
+      //config.setExportInterval(Duration.fromMillis(500)); // How do we ensure a push before shutdown?
+      config.setConstantLabels(getOpenCensusDefaultLabels());
+
+      StackdriverStatsExporter.createAndRegister(config.build());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
